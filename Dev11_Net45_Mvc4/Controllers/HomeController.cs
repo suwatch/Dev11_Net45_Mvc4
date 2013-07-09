c11b74984f6ad7a9d8fc636dbf5478368c0d9328
Suwath Ch <suwatch@microsoft.com>
2013-07-09 11:30:58 -0700

    add ip

--- Dev11_Net45_Mvc4/Controllers/HomeController.cs
+++ Dev11_Net45_Mvc4/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace Dev11_Net45_Mvc4.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Dev11 Mvc4 application targetting Fx4.5";
+            ViewBag.Message = "Dev11 Mvc4 application targetting Fx4.5 (WorkerIP: " + HttpContext.Request.ServerVariables["LOCAL_ADDR"] + ")";
 
             return View();
         }
